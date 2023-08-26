5121f213e2d964a71b6c804fe87c90481953ac65
PRussell961 <Prussel961@gmail.com>
2023-08-26 17:33:38 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import AboutProject from './components/AboutProject';
+import AboutUs from './components/AboutUs';
+import Play from './components/Play';
+import NavigationBar from './components/NavigationBar';
 import './App.css';
-
+import {Route, Routes} from "react-router-dom";
 function App() {
+
+  //Routing
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <NavigationBar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Play />} />
+            <Route path="Play" element={<Play />} />
+            <Route path="AboutProject" element={<AboutProject />} />
+            <Route path="AboutUs" element={<AboutUs />} />
+         </Routes>
+        </div>
     </div>
   );
 }
